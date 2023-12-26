a267f2ce9ef1649157097a1b870feaa230e6a715
Line-em <alineemily@hotmail.com>
2023-12-26 23:23:08 +0000

    small fixes

--- src/utils/rating.js
+++ src/utils/rating.js
@@ -5,12 +5,12 @@ export const getStars = (rating) => {
     for (let i = rating; i >= 0.5; i--) {
         if (i <= 0.80) {
             output.push(
-                `<img src="/assets/star-half-fill.svg" class="text-gradient"  alt="Half Star"/>`
+                `<img src="/assets/star-half-fill.svg" style="border: none" alt="Half Star"/>`
                 // `<i class="ph-star-half-fill ph-lg text-gradient" aria-hidden="true"></i>`
             );
         } else {
             output.push(
-                `<img src="/assets/star-fill.svg" class="text-gradient"  alt="Full Star"/>`
+                `<img src="/assets/star-fill.svg" style="border: none" alt="Full Star"/>`
                 // `<i class="ph-star-fill ph-lg text-gradient" aria-hidden="true"></i>`
             );
         }
@@ -18,7 +18,7 @@ export const getStars = (rating) => {
 
     for (let i = 5 - rating; i >= 1; i--)
         // output.push(`<i class="ph-star ph-lg text-gradient" aria-hidden="true"></i>`);
-        output.push(`<img src="/assets/star.svg" class="text-gradient"  alt="Half Star"/>`);
+        output.push(`<img src="/assets/star.svg" style="border: none" alt="Half Star"/>`);
 
 
     return output.join("");

c7182db2ffff9ec749083c3d1314a6ccb10d6f54
Line-em <alineemily@hotmail.com>
2023-12-26 23:13:55 +0000

    small fixes

--- src/utils/rating.js
+++ src/utils/rating.js
@@ -1,29 +1,33 @@
 export const getStars = (rating) => {
-	rating = Math.round(rating * 2) / 2;
-	let output = [];
+    rating = Math.round(rating * 2) / 2;
+    let output = [];
 
-	for (let i = rating; i >= 0.5; i--) {
-		if (i <= 0.80) {
-			output.push(
-				`<i class="ph-star-half-fill ph-lg text-gradient" aria-hidden="true"></i>`
-			);
-		} else {
-			output.push(
-				`<i class="ph-star-fill ph-lg text-gradient" aria-hidden="true"></i>`
-			);
-		}
-	}
+    for (let i = rating; i >= 0.5; i--) {
+        if (i <= 0.80) {
+            output.push(
+                `<img src="/assets/star-half-fill.svg" class="text-gradient"  alt="Half Star"/>`
+                // `<i class="ph-star-half-fill ph-lg text-gradient" aria-hidden="true"></i>`
+            );
+        } else {
+            output.push(
+                `<img src="/assets/star-fill.svg" class="text-gradient"  alt="Full Star"/>`
+                // `<i class="ph-star-fill ph-lg text-gradient" aria-hidden="true"></i>`
+            );
+        }
+    }
 
-	for (let i = 5 - rating; i >= 1; i--)
-		output.push(`<i class="ph-star ph-lg text-gradient" aria-hidden="true"></i>`);
+    for (let i = 5 - rating; i >= 1; i--)
+        // output.push(`<i class="ph-star ph-lg text-gradient" aria-hidden="true"></i>`);
+        output.push(`<img src="/assets/star.svg" class="text-gradient"  alt="Half Star"/>`);
 
-	return output.join("");
+
+    return output.join("");
 };
 
 export const getAverageStars = (ratings) => {
-	const totalScore = ratings.reduce((acc, curr) => acc + curr, 0);
-	// const totalScore = ratings.reduce((acc, book) => acc + book.frontmatter.score, 0)
-	// return Math.round(totalScore / ratings.length);
-	const average = totalScore / ratings.length;
-	return parseFloat(average.toFixed(1));
+    const totalScore = ratings.reduce((acc, curr) => acc + curr, 0);
+    // const totalScore = ratings.reduce((acc, book) => acc + book.frontmatter.score, 0)
+    // return Math.round(totalScore / ratings.length);
+    const average = totalScore / ratings.length;
+    return parseFloat(average.toFixed(1));
 };

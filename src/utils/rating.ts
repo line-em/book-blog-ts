be9db8d8329eadd20e25c4bc08e1bffff44fb927
Aline Emily <alineemily.dev@gmail.com>
2024-07-17 11:56:41 +0100

    rating fix

--- src/utils/rating.ts
+++ src/utils/rating.ts
@@ -1,21 +1,18 @@
-export const getStars = (rating:number): string => {
+export const getStars = (rating: number): string => {
 	rating = Math.round(rating * 2) / 2
 	let output = []
 
-	for (let i = rating; i >= 0.5; i--) {
-		if (i <= 0.8) {
-			output.push(
-				`<img src="/assets/star-half-fill.svg" style="border: none" alt="Half Star"/>`
-			)
-		} else {
-			output.push(
-				`<img src="/assets/star-fill.svg" style="border: none" alt="Full Star"/>`
-			)
-		}
+	for (let i = 1; i <= rating; i++) {
+		output.push(`<img src="/assets/star-fill.svg" style="border: none" alt="Full Star"/>`)
 	}
 
-	for (let i = 5 - rating; i >= 1; i--)
-		output.push(`<img src="/assets/star.svg" style="border: none" alt="Half Star"/>`)
+	if (rating % 1 !== 0) {
+		output.push(`<img src="/assets/star-half-fill.svg" style="border: none" alt="Half Star"/>`)
+	}
+
+	for (let i = Math.ceil(rating); i < 5; i++) {
+		output.push(`<img src="/assets/star.svg" style="border: none" alt="Empty Star"/>`)
+	}
 
 	return output.join('')
 }

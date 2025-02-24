12b5cb5c831e3ce23380de4c4b58cb2660f944ac
Aline Emily <1231866@isep.ipp.pt>
2025-02-24 22:28:33 +0000

    Design fixes

--- src/components/YearButtons.jsx
+++ src/components/YearButtons.jsx
@@ -9,7 +9,7 @@ const YearButtons = ({ currentYear = null, isReview }) => {
       <a
         href={`/index.html`}
         className={`${hasFilter ? "" : "inactive pointer-events-none opacity-50"} 
-			flex items-center px-2 py-2 text-zinc-700 rounded-xl shadow-md bg-slate-50 gap-4 w-full self-center hover:text-teal-500`}
+			flex items-center px-2 py-2 text-zinc-700 rounded-xl shadow-sm bg-slate-50 gap-4 w-full self-center hover:text-teal-500`}
       >
         <Books size={24} color="var(--pink)" weight={"duotone"} />
         <span className={`font-medium`}>All Books</span>
@@ -17,7 +17,7 @@ const YearButtons = ({ currentYear = null, isReview }) => {
       {validYears.map(curr => (
         <a
           className={`${currentYear !== curr ? "" : "inactive pointer-events-none opacity-50"} 
-		flex items-center p-1 sm:px-4 sm:py-2 text-zinc-700 rounded-xl sm:shadow-md sm:bg-slate-50 gap-4 hover:text-teal-500`}
+		flex items-center p-1 sm:px-4 sm:py-2 text-zinc-700 rounded-xl sm:shadow-sm sm:bg-slate-50 gap-4 hover:text-teal-500`}
           href={`/${Number(curr)}`}
         >
           <BookOpenText
